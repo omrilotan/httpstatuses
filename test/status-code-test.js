658592acce7f75405122f3cf3a5784450729fadd
Casey Robinson <kc@rampantmonkey.com>
2014-05-21 11:55:47 -0400

    scaffolding for testing

---
+++ test/status-code-test.js
@@ -0,0 +1,19 @@
+var assert = require('assert'),
+    httpStatus = require('../')
+
+describe('HTTP Status', function () {
+  describe('Informational', function() {
+  })
+
+  describe('Success', function() {
+  })
+
+  describe('Redirection', function() {
+  })
+
+  describe('Client Error', function() {
+  })
+
+  describe('Server Error', function() {
+  })
+})
