e70346f98d69e3ccb61203cce5168356ae2f9244
Casey Robinson <kc@rampantmonkey.com>
2014-05-21 20:14:20 -0400

    implement success codes

--- index.js
+++ index.js
@@ -1,3 +1,14 @@
 exports.continue        = 100
 exports.switchProtocols = 101
 exports.processing      = 102
+
+exports.ok               = 200
+exports.created          = 201
+exports.accepted         = 202
+exports.nonAuthoritative = 203
+exports.noContent        = 204
+exports.resetContent     = 205
+exports.partialContent   = 206
+exports.multiStatus      = 207
+exports.alreadyReported  = 208
+exports.imUsed           = 226
--- test/status-code-test.js
+++ test/status-code-test.js
@@ -17,6 +17,36 @@ describe('HTTP Status', function () {
   })
 
   describe('Success', function() {
+    it('200', function () {
+      assert.equal(httpStatus.ok, 200)
+    })
+    it('201', function () {
+      assert.equal(httpStatus.created, 201)
+    })
+    it('202', function () {
+      assert.equal(httpStatus.accepted, 202)
+    })
+    it('203', function () {
+      assert.equal(httpStatus.nonAuthoritative, 203)
+    })
+    it('204', function () {
+      assert.equal(httpStatus.noContent, 204)
+    })
+    it('205', function () {
+      assert.equal(httpStatus.resetContent, 205)
+    })
+    it('206', function () {
+      assert.equal(httpStatus.partialContent, 206)
+    })
+    it('207', function () {
+      assert.equal(httpStatus.multiStatus, 207)
+    })
+    it('208', function () {
+      assert.equal(httpStatus.alreadyReported, 208)
+    })
+    it('226', function () {
+      assert.equal(httpStatus.imUsed, 226)
+    })
   })
 
   describe('Redirection', function() {
